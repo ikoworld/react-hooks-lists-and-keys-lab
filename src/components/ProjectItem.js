169bbeda088ad8464261380bed28ed78f21c7501
aboura <bouchrabichou638@gmail.com>
2023-09-11 21:24:26 +0200

    completed assignment

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -7,6 +7,7 @@ function ProjectItem({ name, about, technologies }) {
       <p>{about}</p>
       <div className="technologies">
         {/* render a <span> for each technology in the technologies array */}
+        {technologies.map((technologie,index)=><span key={index}>{technologie}</span>)}
       </div>
     </div>
   );
